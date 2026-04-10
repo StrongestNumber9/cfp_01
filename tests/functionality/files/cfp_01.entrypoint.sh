278207c3f7761a3f81d1b403aba8a77609ead84d
StrongestNumber9 <16169054+StrongestNumber9@users.noreply.github.com>
2026-04-10 10:54:15 +0300

    Fixes?

--- tests/functionality/files/cfp_01.entrypoint.sh
+++ tests/functionality/files/cfp_01.entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "success" | tee "/return/$(hostname -s).return";
 echo "Powering off!";
-( sleep 5; systemctl start poweroff.target; ) &
+systemctl start poweroff.target;
--- tests/functionality/files/ipa_01.entrypoint-override.sh
+++ tests/functionality/files/ipa_01.entrypoint-override.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # This file is just to override /ipa_01.entrypoint.sh as we do not use full ipa here.
-systemctl exit 0;
+echo "success" | tee "/return/$(hostname -s).ipa.return";
+exit 0;
