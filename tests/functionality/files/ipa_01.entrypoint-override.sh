7e9bbb95dc775709430cf5be4049d4a56757e8ea
StrongestNumber9 <16169054+StrongestNumber9@users.noreply.github.com>
2026-04-10 10:58:23 +0300

    Test

--- tests/functionality/files/ipa_01.entrypoint-override.sh
+++ tests/functionality/files/ipa_01.entrypoint-override.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This file is just to override /ipa_01.entrypoint.sh as we do not use full ipa here.
 echo "success" | tee "/return/$(hostname -s).ipa.return";
-exit 0;
+systemctl exit;
